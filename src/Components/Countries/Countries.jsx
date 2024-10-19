c957c8256e8be77cc4aa494b17234f1f85529f18
Foisal Ahmed <shakhfoisal99@gmail.com>
2024-10-19 22:32:32 +0600

    country vising loading

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,19 +1,37 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Country from "../Country/Country";
+import "./countries.css";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [visitedcountry, setVisitedcountry] = useState([])
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
+  const handleVisitedContry = (country) => {
+    console.log(country);
+  }
+
+
+
+
   return (
     <div>
-      <h3>Countries: {countries.length}</h3>
-      {countries.map((country) => (
-        <Country key={country.cca3} country={country}></Country>
-      ))}
+      <h2>Countries: {countries.length}</h2>
+      <div>
+      <h5>Visited Countries: </h5>
+      <ul>
+
+      </ul>
+      </div>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country  key={country.cca3} country={country} handleVisitedContry={handleVisitedContry}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,13 +1,27 @@
-import './country.css'
-const Country = ({ country }) => {
-  const { name, flags } = country;
+import { useState } from "react";
+import "./country.css";
+const Country = ({ country, handleVisitedContry }) => {
+  const { name, flags, area, region, population } = country;
   console.log(country);
-  return (
-    <div className='country'>
-      <h3>Name:  {name?.common} </h3>
-      <img src={flags.png} alt="" />
-    </div>
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  };
 
+  console.log(handleVisitedContry);
+  return (
+    <div className={`country ${visited ? "visited" : "non-visited"}`}>
+      <h2>Name: {name?.common} </h2>
+      <img src={flags.png} alt="" />
+      <h3>Area: {area}</h3>
+      <h3>Nationlaty: {region}</h3>
+      <h3>population: {population}</h3>
+      <button>
+        Mark as visited
+      </button>
+      <button onClick={handleVisited}>{visited ? "Visited" : "visit"}</button>
+      {visited ? "I have visited this country." : "I want to visit"}
+    </div>
   );
 };
 
